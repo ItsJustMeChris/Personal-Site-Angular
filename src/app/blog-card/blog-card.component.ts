247040fceb2d29bc8c3031f7a96c0a5c66485204
ItsJustMeChris <christian.mcgravey@gmail.com>
2020-05-22 23:20:53 -0400

    Update blog card to show read time and time since posted

--- src/app/blog-card/blog-card.component.ts
+++ src/app/blog-card/blog-card.component.ts
@@ -19,4 +19,30 @@ export class BlogCardComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  getReadTime(): number {
+    return Math.ceil(this.content.split(' ').length / 200);
+  }
+
+  timeAgo(): string {
+    const now = new Date().getTime();
+    const then = new Date(this.postTime).getTime();
+    const seconds = Math.floor((now - then) / 1000);
+    if (Math.floor(seconds / 31536000) >= 1) {
+      return `${Math.floor(seconds / 31536000)} years ago`;
+    }
+    if (Math.floor(seconds / 2592000) >= 1) {
+      return `${Math.floor(seconds / 2592000)} months ago`;
+    }
+    if (Math.floor(seconds / 86400) >= 1) {
+      return `${Math.floor(seconds / 86400)} days ago`;
+    }
+    if (Math.floor(seconds / 3600) >= 1) {
+      return `${Math.floor(seconds / 3600)} hours ago`;
+    }
+    if (Math.floor(seconds / 60) >= 1) {
+      return `${Math.floor(seconds / 60)} minutes ago`;
+    }
+    return `${Math.floor(seconds)} seconds ago`;
+  }
+
 }
