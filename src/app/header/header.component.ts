f52b0ff3e2b7a19a5b7159f215ed73df3456d34a
Christian McGravey <christian@amazing.com>
2020-10-29 01:44:53 +0400

    Adds snow playing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  public song: any = null;
+  public artistsString: any = null;
+  public ticker: any = null;
 
-  constructor() { }
+  constructor(private spotifyService: SpotifyService) { }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    if (this.ticker) {
+      clearInterval(this.ticker);
+    }
+  }
+
+  async fetchSong() {
+    const data = await this.spotifyService.getNowPlaying();
+    if (!data?.is_playing) {
+      this.song = null;
+      this.artistsString = null;
+      return false;
+    }
+    this.song = data?.item;
+    this.artistsString = this.song.artists.map((artist) => artist.name).join(', ');
+  }
+
+  async ngOnInit() {
+    this.fetchSong();
+    setInterval(() => {
+      this.fetchSong();
+    }, 60000);
   }
 
 }
---
+++ src/app/spotify.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+const endpoint = "https://api.spotify.com/v1";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SpotifyService {
+  private cache = [];
+  constructor(private http: HttpClient) { }
+
+  async getNowPlaying(): Promise<any> {
+    const songInfo = await this.http.get<any>(`${endpoint}/me/player`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer BQDN7EOGowYW3bSmIYyHxe0FIjR8pCQT8_pDI8wdlYEkTHah6I2xPk5gaLgsvYOqGcDTUBzB78r--4mwqU6PSWjNmgQ7Mg8idP5XBdOM5l-2gK2FHXICfww4qVgEeufFGSqptbd5hxDCV8dGUFSaFvCuYwFcL2bQiQ'
+      }
+    }).toPromise();
+    return songInfo;
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      return of(result as T);
+    }
+  }
+}
